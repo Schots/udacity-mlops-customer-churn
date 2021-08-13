5e300cc83a97a67e3c925c4a7ceeb881df677304
Maykon Schots <maykonschots@gmail.com>
2021-08-13 15:16:08 -0300

    Add conditional flows about presence or absence of categorial/numerical features

--- eda.py
+++ eda.py
@@ -1,6 +1,6 @@
 from churn_library import (import_data,eda_single_plot,eda_grid_plot)
 
-from utils import read_config
+from utils import read_config,has_categorical,has_numerical
 
 
 def run_eda():
@@ -8,46 +8,52 @@ def run_eda():
 
     data = import_data(config["DATA"]["PATH"])
 
+
     eda_single_plot(data=data,
                         kind="target_distribution",
                         title=config["EDA_TARGET"]["TARGET_DISTRIBUTION_TITLE"],
                         xlabel="% of observations",
                         )
 
-    eda_single_plot(data=data,
+    if has_numerical:
+        
+        eda_single_plot(data=data,
                         kind="target_correlations",
                         title=config["EDA_TARGET"]["TARGET_CORRELATIONS_TITLE"],
                         xlabel="% of observations",
                         )
 
-    eda_single_plot(data=data,
+        eda_single_plot(data=data,
                         kind="feature_correlations",
                         title=config["EDA_NUMERICAL"]["NUMERICAL_FEATURES_CORRELATIONS_TITLE"],
                                 )
 
-    eda_single_plot(data=data,
-                        kind="categorical_cardinality",
-                        title=config["EDA_CATEGORICAL"]["CATEGORICAL_CARDINALITY_TITLE"],
-                            )
-
-    eda_grid_plot(data=data,
-            gridtitle=config["EDA_CATEGORICAL"]["GRID_TITLE"],
-            kind="Rate",
-            )
-
-    eda_grid_plot(data=data,
+        eda_grid_plot(data=data,
             gridtitle=config["EDA_NUMERICAL"]["GRID_TITLE"],
             kind="distribution",
             )
 
-    eda_grid_plot(data=data,
+        eda_grid_plot(data=data,
             kind="histogram",
             gridtitle=config["EDA_NUMERICAL"]["HISTOGRAM_TITLE"])
 
             
-    eda_grid_plot(data=data,
+        eda_grid_plot(data=data,
             kind="strip",
             gridtitle=config["EDA_NUMERICAL"]["STRIPPLOT_TITLE"])
+
+    
+    if has_categorical:
+
+        eda_single_plot(data=data,
+                        kind="categorical_cardinality",
+                        title=config["EDA_CATEGORICAL"]["CATEGORICAL_CARDINALITY_TITLE"],
+                            )
+
+        eda_grid_plot(data=data,
+            gridtitle=config["EDA_CATEGORICAL"]["GRID_TITLE"],
+            kind="Rate",
+            )
 
 if __name__ == "__main__":
     run_eda()
